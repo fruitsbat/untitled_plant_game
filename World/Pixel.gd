612cb6652bac5e8454422ebef385971ae25b9681
zoe <z8V54gHftevp7LeRVyys$8@DosKq^FeQKUWU!WGpTyiLaxGojNdgnWkaWiv673Twg$aPm&hU>
2021-12-04 01:20:48 +0100

    all the things

---
+++ World/Pixel.gd
@@ -0,0 +1,8 @@
+extends ColorRect
+var empty = true
+var position = Vector2()
+
+export var lighter_green = Color("#91e369")
+export var light_green = Color("#56c157")
+export var medium_green = Color("#46a65c")
+export var dark_green = Color("#358859")
--- World/Plant.gd
+++ World/Plant.gd
@@ -1,23 +1,31 @@
 extends Node2D
 export var size = Vector2(128, 144)
 # 2 dimensional array y and x
-var pixels_y = []
+var pixels_x = []
 
 func _ready():
 	call_deferred("spawn_pixels")
+	call_deferred("plant_seed")
 
 func spawn_pixels():
 	for n in size.x:
-		var pixels_x = []
+		var pixels_y = []
 		for i in size.y:
 			var Main = get_tree().current_scene
 			var Pixel = preload("res://World/Pixel.tscn")
 			var pixel = Pixel.instance()
 			# set position
 			pixel.rect_global_position = Vector2(n, i)
+			pixel.position = Vector2(n, i)
 			# show in scene
 			Main.add_child(pixel)
 			# initilize array
 			pixels_y.append(pixel)
 		pixels_x.append(pixels_y)
 
+func plant_seed():
+	var rect = pixels_x[size.x / 2][size.y - 8]
+	rect.color = rect.light_green
+
+func grow():
+	pass
